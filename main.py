b5caaee3f28287f651e97457e2c1b0023dc60399
shams-lune <82583638+shams-lune@users.noreply.github.com>
2021-04-15 18:18:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ async def codecreateur(ctx):
 
 @bot.event
 async def on_member_join(member):
+        print("Quelqu'un a rejoins")
         channel = bot.get_channel(683769890888351811)
         embed=discord.Embed(title="Bienvenue", url="https://www.twitch.tv/salvecko", description="Tout le monde te souhaite la bienvenue dans l':ekip112:SalveckOY", color=0xffffff)
         embed.set_author(name="Bienvenue !!", url="https://www.twitch.tv/salvecko", icon_url="https://static-cdn.jtvnw.net/jtv_user_pictures/7cbf35e6-49de-4c76-81f3-52a9e63ae1e0-profile_image-70x70.png")
